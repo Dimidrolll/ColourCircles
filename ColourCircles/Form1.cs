638fa81db37771a2eee1d81f6bc1b4817684b0c3
Dima Bobrov <bda29041998@gmail.com>
2017-11-04 22:42:46 +0400

    Саша удалил

--- ColourCircles/Form1.cs
+++ ColourCircles/Form1.cs
@@ -13,9 +13,6 @@ namespace ColourCircles
     public partial class Form1 : Form
     {
         const int max_count = 100;
-        int count_of_threads = -1;
-        CircleTemlate[] circleTemplate = new CircleTemlate[max_count];
-        CircleDrawer[] circleDrawer = new CircleDrawer[max_count];
         public Form1()
         {
             InitializeComponent();
@@ -29,26 +26,14 @@ namespace ColourCircles
 
         private void addThread_Click(object sender, EventArgs e)
         {
-            ++count_of_threads;
-            Graphics g = this.CreateGraphics();
-            circleTemplate[count_of_threads] = new CircleTemlate(colorDialog1.Color, (int)nudRadius.Value);
-            circleDrawer[count_of_threads] = new CircleDrawer(circleTemplate[count_of_threads], (int)nudRefreshInt.Value, g);
-
-            circleDrawer[count_of_threads].thread.Name = circleTemplate[count_of_threads].color.Name;
-            listOfThreads.Items.Add(circleDrawer[count_of_threads]);
-
-            circleDrawer[count_of_threads].Start();
+      
+  
         }
 
 
         private void removeThread_Click(object sender, EventArgs e)
         {
-            if (listOfThreads.SelectedIndex != -1)
-            {
-             
-                --count_of_threads;
 
-            }
         }
 
         private void colorButton_Click(object sender, EventArgs e)
@@ -59,28 +44,16 @@ namespace ColourCircles
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            /*for (int i = 0; i < count_of_threads; ++i)
-            {
-                circleDrawer[i].End();
-                circleDrawer[i].waitEnd();
-            }*/
+
 
         }
         private void suspendButton_Click(object sender, EventArgs e)
         {
-            if (listOfThreads.SelectedIndex != -1)
-            {
-                int temp = listOfThreads.SelectedIndex;
-                circleDrawer[count_of_threads].Stop();
-            }
+
         }
         private void resumeButton_Click(object sender, EventArgs e)
         {
-            if (listOfThreads.SelectedIndex != -1)
-            {
-                int temp = listOfThreads.SelectedIndex;
-                circleDrawer[count_of_threads].Resume();
-            }
+
         }
     }
 }
